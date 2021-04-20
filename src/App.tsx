8d5d8ea83a993a39ee6408c558cec92cfc942e35
Gean Gomes <geansg4@gmail.com>
2021-04-19 23:00:40 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 32,
+  },
+});
+
+export default App;
